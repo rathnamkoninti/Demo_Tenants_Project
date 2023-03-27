c9a49076f897a3b52d3eadd3f82e1558e6aa9958
rathnamkoninti <rathnamkoninti@gmail.com>
2023-03-27 22:18:17 +0530

    sfsdfdsfsd

--- public/views.py
+++ public/views.py
@@ -6,6 +6,20 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 @api_view(['GET','POST'])
+def greet(request):
+    if request.method== 'GET':
+        greet = 'Public Tenant'
+        return Response(greet)
+
+@api_view(['GET','POST'])
+def greet123(request):
+    if request.method== 'GET':
+        ss='fdlksfjksdlfjdjkflsdjkfjdkslfdklsfkldsfjkdsjklfdsfdskjnfjsdknjsadngjdsngdsm,g'
+        greet = 'gfdklgnfdlkgnfdk Tenant Git checkupgdsjghsjkgnsadghjksdgsahd'
+        return Response(greet) gfdglkfkdgg
+
+
+@api_view(['GET','POST'])
 def greetings(request):
     if request.method== 'GET':
         greet = 'Private Tenant'

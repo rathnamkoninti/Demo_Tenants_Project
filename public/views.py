7ef8356e2f98b9dc639f3d0cf256ba9d031cdfd8
rathnam koninti <38881814+rathnamkoninti@users.noreply.github.com>
2023-03-27 22:18:29 +0530

    Update views.py

--- public/views.py
+++ public/views.py
@@ -17,3 +17,16 @@ def greet123(request):
         ss='fdlksfjksdlfjdjkflsdjkfjdkslfdklsfkldsfjkdsjklfdsfdskjnfjsdknjsadngjdsngdsm,g'
         greet = 'gfdklgnfdlkgnfdk Tenant Git checkupgdsjghsjkgnsadghjksdgsahd'
         return Response(greet) gfdglkfkdgg
+
+
+
+@api_view(['GET','POST'])
+def greetings3(request):
+    if request.method== 'GET':
+        with connection.cursor() as cursor:
+            cursor.execute(''' 
+                select clg.name as college_name,clg.address,clg.mobile as college_mobile,
+                std.student_name  from private_college clg
+                inner join private_student std on std.college_id_id=clg.id  ''')
+            data=dictfetchall(cursor)
+            return Response(data)

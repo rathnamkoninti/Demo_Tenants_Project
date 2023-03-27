d49b57bfeff0227bcaebf341e4c4051ee9cf7725
rathnamkoninti <rathnamkoninti@gmail.com>
2023-03-27 22:13:15 +0530

    some random changes

--- public/views.py
+++ public/views.py
@@ -6,14 +6,37 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 @api_view(['GET','POST'])
-def greet(request):
+def greetings(request):
     if request.method== 'GET':
-        greet = 'Public Tenant'
+        greet = 'Private Tenant'
         return Response(greet)
 
+
 @api_view(['GET','POST'])
-def greet123(request):
+def greetings1(request):
     if request.method== 'GET':
-        ss='fdlksfjksdlfjdjkflsdjkfjdkslfdklsfkldsfjkdsjklfdsfdskjnfjsdknjsadngjdsngdsm,g'
-        greet = 'gfdklgnfdlkgnfdk Tenant Git checkupgdsjghsjkgnsadghjksdgsahd'
-        return Response(greet) gfdglkfkdgg
+        with connection.cursor() as cursor:
+            cursor.execute(''' select * from private_publictable1''')
+            data=dictfetchall(cursor)
+            return Response(data)
+
+
+@api_view(['GET','POST'])
+def greetings2(request):
+    if request.method== 'GET':
+        with connection.cursor() as cursor:
+            cursor.execute(''' select * from private_college''')
+            data=dictfetchall(cursor)
+            return Response(data)
+
+
+@api_view(['GET','POST'])
+def greetings3(request):
+    if request.method== 'GET':
+        with connection.cursor() as cursor:
+            cursor.execute(''' 
+                select clg.name as college_name,clg.address,clg.mobile as college_mobile,
+                std.student_name  from private_college clg
+                inner join private_student std on std.college_id_id=clg.id  ''')
+            data=dictfetchall(cursor)
+            return Response(data)
